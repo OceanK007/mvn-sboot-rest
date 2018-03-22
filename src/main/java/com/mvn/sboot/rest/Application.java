fbbe9422f1b8fa10bafd1079d99db42899298881
Ocean <oceank007@gmail.com>
2018-03-22 14:19:45 +0530

    Modification

--- src/main/java/com/mvn/sboot/rest/Application.java
+++ src/main/java/com/mvn/sboot/rest/Application.java
@@ -1,7 +1,10 @@
 package com.mvn.sboot.rest;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 public class Application 
@@ -11,3 +14,21 @@ public class Application
 		SpringApplication.run(Application.class, args);
 	}
 }
+
+/**
+* The @EnableAutoConfiguration annotation enables the auto-configuration of Spring ApplicationContext by scanning the classpath components and registers the beans that are matching various conditions. These bean classes will be annotated with @Configuration annotation.
+* Typically AutoConfiguration classes are annotated with @Configuration to mark it as a Spring configuration class and annotated with @EnableConfigurationProperties to bind the customization properties and one or more Conditional bean registration methods.
+* SpringBoot provides various AutoConfiguration classes in spring-boot-autoconfigure-{version}.jar, which are responsible for registering various components.
+**/
+/** Or use this configurations **/
+/*@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+public class Application
+{
+	public static void main(String[] args) 
+	{
+		SpringApplication.run(Application.class, args);
+	}
+}
+*/
